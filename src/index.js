818bc854827aeb287777a4e9088b4a24236a7129
ankmsn <khomstyle@gmail.com>
2023-01-19 14:25:52 +0000

    init: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  for (i=0; i<bracketsConfig.length; i++){
+    let newStr = str;
+    str = str.replaceAll(bracketsConfig[i][0]+bracketsConfig[i][1], '')
+    if (str != newStr) {i=-1} else if (str.length === 0) {return true}
+  }
+  return false
+  }
+
